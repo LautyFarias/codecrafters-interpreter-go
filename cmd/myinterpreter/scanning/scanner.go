e7bcd0b5bc9671df5386adea9c9ccd8942b4307a
Lautaro Farias <farias.lautaro002@gmail.com>
2024-07-21 01:17:41 -0300

    refactor: Extract line process to a method

--- cmd/myinterpreter/scanning/scanner.go
+++ cmd/myinterpreter/scanning/scanner.go
@@ -28,16 +28,6 @@ func NewScanner(source *os.File) *Scanner {
 	}
 }
 
-func (s *Scanner) next() (rune, error) {
-	nextIndex := s.chI + 1
-
-	if nextIndex >= len(s.line) {
-		return ' ', errors.New("no next rune found")
-	}
-
-	return rune(s.line[nextIndex]), nil
-}
-
 func (s *Scanner) isBuildingString() bool {
 	return s.stringBuilder.Len() > 0 && s.stringBuilder.String()[0] == '"'
 }
@@ -46,99 +36,119 @@ func (s *Scanner) isBuildingNumber() bool {
 	return s.stringBuilder.Len() > 0 && s.stringBuilder.String()[0] != '"'
 }
 
-func (s *Scanner) Scan() {
-	for s.scanner.Scan() {
-		s.lineNumber++
+func (s *Scanner) next() (rune, error) {
+	if s.chI >= len(s.line) {
+		return ' ', errors.New("EOL")
+	}
 
-		s.line = s.scanner.Text()
+	n := s.line[s.chI]
+	s.chI++
 
-		var lexeme string
-		var ch rune
-		skipChar := false
+	return rune(n), nil
+}
 
-	lineIteration:
-		for s.chI, ch = range s.line {
-			if skipChar {
-				skipChar = false
-				continue
-			}
+func (s *Scanner) scanLine() {
+	var lexeme string
 
-			if s.isBuildingString() && ch != '"' {
-				s.stringBuilder.WriteRune(ch)
-				continue
-			}
+lineIteration:
+	for {
+		ch, err := s.next()
 
-			switch ch {
-			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				s.stringBuilder.WriteRune(ch)
-				continue
-			case '.':
-				if s.isBuildingNumber() {
-					str := s.stringBuilder.String()
+		if err != nil {
+			break
+		}
 
-					if strings.ContainsRune(str, ch) {
-						s.reportToken(str)
-						s.stringBuilder.Reset()
+		if s.isBuildingString() && ch != '"' {
+			s.stringBuilder.WriteRune(ch)
+			continue
+		}
 
-						lexeme = string(ch)
-						break
-					}
+		switch ch {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			s.stringBuilder.WriteRune(ch)
+			continue
+		case '.':
+			if s.isBuildingNumber() {
+				str := s.stringBuilder.String()
 
-					next, _ := s.next()
-
-					if unicode.IsNumber(next) {
-						s.stringBuilder.WriteRune(ch)
-						continue
-					}
-
-					s.reportToken(s.stringBuilder.String())
+				if strings.ContainsRune(str, ch) {
+					s.reportToken(str)
 					s.stringBuilder.Reset()
+
+					lexeme = string(ch)
+					break
 				}
 
-				lexeme = string(ch)
-			case '"':
-				s.stringBuilder.WriteRune(ch)
+				next, err := s.next()
 
-				if s.stringBuilder.Len() == 1 {
+				if err == nil && unicode.IsNumber(next) {
+					s.stringBuilder.WriteRune(ch)
 					continue
 				}
 
-				if s.isBuildingString() {
-					lexeme = s.stringBuilder.String()
-					s.stringBuilder.Reset()
-				}
-			case '<', '>', '=', '!':
-				next, _ := s.next()
-
-				lexeme = string(ch)
-
-				if next == '=' {
-					lexeme += string(next)
-					skipChar = true
-				}
-			case '/':
-				next, err := s.next()
-
-				if err != nil {
-					lexeme = string(ch)
-				}
-
-				if next == '/' {
-					break lineIteration
-				}
-			case BlankToken, TabToken:
-				continue
-			default:
-				lexeme = string(ch)
-			}
-
-			if s.isBuildingNumber() {
 				s.reportToken(s.stringBuilder.String())
 				s.stringBuilder.Reset()
 			}
 
-			s.reportToken(lexeme)
+			lexeme = string(ch)
+		case '"':
+			s.stringBuilder.WriteRune(ch)
+
+			if s.stringBuilder.Len() == 1 {
+				continue
+			}
+
+			if s.isBuildingString() {
+				lexeme = s.stringBuilder.String()
+				s.stringBuilder.Reset()
+			}
+		case '<', '>', '=', '!':
+			next, _ := s.next()
+
+			lexeme = string(ch)
+
+			if next != '=' {
+				s.chI--
+				break
+			}
+
+			lexeme += string(next)
+		case '/':
+			next, err := s.next()
+
+			if err != nil {
+				lexeme = string(ch)
+				break
+			}
+
+			if next == '/' {
+				break lineIteration
+			}
+
+			lexeme = string(ch)
+			s.chI--
+		case BlankToken, TabToken:
+			continue
+		default:
+			lexeme = string(ch)
 		}
+
+		if s.isBuildingNumber() {
+			s.reportToken(s.stringBuilder.String())
+			s.stringBuilder.Reset()
+		}
+
+		s.reportToken(lexeme)
+	}
+}
+
+func (s *Scanner) Scan() {
+	for s.scanner.Scan() {
+		s.lineNumber++
+		s.line = s.scanner.Text()
+
+		s.scanLine()
+		s.chI = 0
 
 		if s.isBuildingNumber() {
 			s.reportToken(s.stringBuilder.String())
