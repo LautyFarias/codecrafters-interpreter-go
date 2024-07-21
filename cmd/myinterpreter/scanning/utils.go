404bf0fce78eb1df85da7e47e56018c0f19144ab
Lautaro Farias <farias.lautaro002@gmail.com>
2024-07-21 12:24:01 -0300

    refactor: Remove unused code in scanning/utils

--- cmd/myinterpreter/scanning/utils.go
+++ cmd/myinterpreter/scanning/utils.go
@@ -1,20 +1,8 @@
 package scanning
 
 import (
-	"regexp"
 	"strconv"
 )
-
-func toNumber(s string) (match string, ok bool) {
-	re := regexp.MustCompile(`^\d+(\.\d+)?`)
-	matches := re.FindStringSubmatch(s)
-
-	if len(matches) == 0 {
-		return "", false
-	}
-
-	return matches[0], true
-}
 
 func isNumeric(s string) bool {
 	_, err := strconv.ParseFloat(s, 64)
