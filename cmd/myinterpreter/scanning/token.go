2dc179fc3b8d79ac6aeccb4fb5f5603640be7672
Lautaro Farias <farias.lautaro002@gmail.com>
2024-07-24 00:48:01 -0300

    fix: Allows underscore as part of an identifier

--- cmd/myinterpreter/scanning/token.go
+++ cmd/myinterpreter/scanning/token.go
@@ -190,6 +190,6 @@ func getLiteral(lexeme string, tt TokenType) string {
 
 func IsIdentifier(lexeme string) bool {
 	return !strings.ContainsFunc(lexeme, func(r rune) bool {
-		return unicode.IsSymbol(r)
+		return unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsSpace(r) || r == '_'
 	})
 }
